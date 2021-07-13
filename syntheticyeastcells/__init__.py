b737e31c3e88b3228cb3ef5edb47e8aa8a30e52c
DanKaptijn <33229007+DanKaptijn@users.noreply.github.com>
2021-07-13 11:31:08 +0200

    Update __init__.py

--- syntheticyeastcells/__init__.py
+++ syntheticyeastcells/__init__.py
@@ -274,10 +274,10 @@ def create_samples(n_images, n_cells_per_image=100,
                 new_bud = {
                     'centerx':(x+r+bud_radius).astype(numpy.int), 
                     'centery':(y).astype(numpy.int), 
-                    'radius0':randint_range(bud_radius,bud_radius),
-                    'radius1':(bud_radius * r1_factor).astype(numpy.int),
-                    'angle':  randint_range(0, 360),
-                    'white-outside': numpy.random.rand(n) < p_white_outside}
+                    'radius0':(randint_range(bud_radius,bud_radius))[0],
+                    'radius1':((bud_radius * r1_factor).astype(numpy.int))[0],
+                    'angle':(randint_range(0, 360))[0],
+                    'white-outside': (numpy.random.rand(n) < p_white_outside)[0]}
                 print(new_bud)
                 cells = cells.append(new_bud, ignore_index=True)
         ### End Dan Code
