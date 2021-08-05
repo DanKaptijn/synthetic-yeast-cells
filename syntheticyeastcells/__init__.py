99192bbead9a5767f75920b30e88637b92bad248
DanKaptijn <33229007+DanKaptijn@users.noreply.github.com>
2021-08-05 13:23:35 +0200

    Update __init__.py

--- syntheticyeastcells/__init__.py
+++ syntheticyeastcells/__init__.py
@@ -168,16 +168,16 @@ def create_sample(size, cells,
         a = cv2.ellipse(a, (x, y), (r0 - 1, r1 - 1), angle, 0, 360, 1., -1)
         b = cv2.ellipse(b, (x, y), (r0 + 2, r1 + 2), angle, 0, 360, 1., -1)
         vacuole_size = randint(2,5)
-        vac = randint(1,10) # variable to decide whether to create a vacuole
-        vac = 1
-        if vac == 1:
+        add_vac = randint(1,10) # variable to decide whether to create a vacuole
+        add_vac = 1
+        if add_vac == 1:
             c = cv2.ellipse(
                     c, (x, y), (round(r0/vacuole_size),round(r0/vacuole_size)), angle, 0, 360, label, -1
                 )
             d = cv2.ellipse(
                     d, (x, y), (round(r0/vacuole_size),round(r0/vacuole_size)), angle, 0, 360, (110,110,110), 2
                 )
-        if vac != 1:
+        if add_vac != 1:
             c = 0
             d = 0
 
